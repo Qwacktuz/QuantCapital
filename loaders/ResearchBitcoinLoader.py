60918ea2cff2522210c288018916f53f6c91b8a4
Qwacktuz <104035252+Qwacktuz@users.noreply.github.com>
2026-02-07 01:49:03 +0100

    fix(api)!: update to v2 api version and simplifying/refactoring logic

--- loaders/ResearchBitcoinLoader.py
+++ loaders/ResearchBitcoinLoader.py
@@ -2,42 +2,42 @@ import io
 import os
 import requests
 import pandas as pd
-from datetime import datetime, timedelta
 from .BaseLoader import BaseLoader
 
 
 class ResearchBitcoinLoader(BaseLoader):
-    base_url = "https://api.researchbitcoin.net/v1/"
+    base_url = "https://api.researchbitcoin.net/v2/"
 
-    def __init__(self, token: str, item: str, index_col: str = "Open time"):
+    def __init__(
+        self,
+        token: str,
+        item: str,
+        index_col: str = "Open time",
+        resolution: str = "d1",
+    ):
         self.token = token
         self.item = item
         self.index_col = index_col
-
-    def _get_date_parameter(self):
-        # The Tier 0 API enables the retrieval of data that is up to 364 days old
-        return (datetime.today() + timedelta(days=1) - timedelta(days=365)).strftime(
-            "%Y-%m-%d"
-        )
+        self.resolution = resolution
 
     def load(self) -> pd.DataFrame:
         endpoint = f"{self.base_url.rstrip('/')}/{self.item.lstrip('/')}"
 
+        headers = {"X-API-Token": self.token}
+
         parameters = {
-            "token": self.token,
-            "date_field": self._get_date_parameter(),
+            "resolution": self.resolution,
             "output_format": "csv",
         }
 
         try:
-            response = requests.get(url=endpoint, params=parameters)
+            response = requests.get(url=endpoint, params=parameters, headers=headers)
             response.raise_for_status()
-        except requests.exceptions.RequestException as e:
-            print(f"Error fetching data: {e}")
+        except requests.exceptions.RequestException:
             raise
 
         df = pd.read_csv(
-            io.StringIO(response.text), parse_dates=["date"], index_col="date"
+            io.StringIO(response.text), parse_dates=["time"], index_col="time"
         )
 
         df.index.name = self.index_col
@@ -54,4 +54,3 @@ class ResearchBitcoinLoader(BaseLoader):
         filepath = os.path.join(output_dir, filename)
 
         df.to_csv(filepath)
-        print(f"Saved to {filepath}")
